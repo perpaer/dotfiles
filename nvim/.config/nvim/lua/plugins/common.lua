9aa64dd425dc7987cc646f42409bcc3b938b7806
Ivan Mrvelj <imrvelj@protonmail.com>
2026-04-08 16:46:07 +0200

    feat(nvim): update lock, tree see hidden files by default

--- nvim/.config/nvim/lua/plugins/common.lua
+++ nvim/.config/nvim/lua/plugins/common.lua
@@ -98,7 +98,16 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
-    }
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
   },
 
   {
